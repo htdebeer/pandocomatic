de0729fe6116f8630876ba89813eef9ce0e4b4f7
Huub de Beer <H.T.de.Beer@gmail.com>
2014-05-09 22:20:44 +0200

    Start of pandoc wrapper based on json output / input of pandoc ast

---
+++ lib/pandocomatic/pandoc/pandoc.rb
@@ -0,0 +1,15 @@
+
+module Pandocomatic
+  module Pandoc
+    class Pandoc
+      def initialize()
+        @metadata = Hash.new
+        @blocks = []
+      end
+
+      def initialize(json)
+        
+      end
+    end
+  end
+end
