2ec3327a5cb74871bc637b3916e9cf9fdbc26efc
Huub de Beer <Huub@heerdebeer.org>
2015-02-14 15:24:30 +0100

    Started metaprogramming pandoc wrapper

--- lib/pandoc/pandoc.rb
+++ lib/pandoc/pandoc.rb
@@ -8,6 +8,26 @@ module Pandoc
 
     end
 
+    def execute
+    end
+
+    def to(format)
+    end
+
+    def from(format)
+    end
+
+    def >>(out)
+    end
+
+
+    # Makes more sense to write a couple of specialize "ruby-like" methods, and
+    # take care of the rest of Pandoc's options through a flexible scheme to
+    # collect options through method_missing.
+
+    def method_missing(name, *args)
+      @config[name.to_s] = args.join ' '
+    end
 
   end
 
