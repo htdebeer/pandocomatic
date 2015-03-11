45cd76043b4bdac5c6b91c7ceb77262eca850c2c
Huub de Beer <H.T.de.Beer@gmail.com>
2015-03-11 10:31:07 +0100

    Fixed bugs that resulted in automatically copying files which should have been converted

--- lib/pandocomatic/configuration.rb
+++ lib/pandocomatic/configuration.rb
@@ -2,16 +2,13 @@ module Pandocomatic
 
   require 'paru/pandoc'
 
-  class Configuration
-
-    def initialize hash = {}
-      @config = {
+  DEFAULT_CONFIG = {
         :recursive => true,
         :follow_links => false,
         :skip => ['.*', 'pandocomatic.yaml'],
         :targets => {
           :markdown => {
-            :pattern => ['*.markdown'],
+            :pattern => ['*.markdown', '*.md'],
             :pandoc => {
               :from => 'markdown',
               :to => 'html5'
@@ -19,7 +16,28 @@ module Pandocomatic
           }
         }
       }
+
+  FORMAT_TO_EXT = {
+    'native' => 'hs',
+    'markdown_strict' => 'markdown',
+    'markdown_phpextra' => 'markdown',
+    'markdown_github' => 'markdown',
+    'html5' => 'html',
+    'docx' => 'docx',
+    'latex' => 'tex'
+  }
+
+  class Configuration
+
+    def initialize hash = DEFAULT_CONFIG
+      @config = {
+        :recursive => true,
+        :follow_links => false,
+        :skip => ['.*'],
+        :targets => {}
+      }
       @convert_patterns = {}
+      reconfigure hash
     end
 
     def reconfigure options
@@ -40,11 +58,11 @@ module Pandocomatic
     end
 
     def skip? src
-      @config[:skip].any? {|pattern| File.fnmatch "**/#{pattern}", src}
+      @config[:skip].any? {|pattern| File.fnmatch pattern, File.basename(src)}
     end
 
     def convert? src
-      @convert_patterns.values.any? {|pattern| File.fnmatch "**/#{pattern}", src}
+      @convert_patterns.values.flatten.any? {|pattern| File.fnmatch pattern, File.basename(src)}
     end
 
     def recursive?
@@ -56,21 +74,24 @@ module Pandocomatic
     end
 
     def determine_target src
-      @convert_patterns.values.select {|pattern| File.fnmatch "**/#{pattern}", src}.first
+      @convert_patterns.select do |target, patterns|
+        patterns.any? {|pattern| File.fnmatch pattern, File.basename(src)}
+      end.keys.first
     end
     
     def convert src, dst_dir
       src_suffix = File.extname src
       src_base = File.basename src, src_suffix
-      target = @config[:targets][determine_target src]
-      dst_suffix = target[:pandoc][:to]
+      target = @config[:targets][determine_target(src)]
+      to = target[:pandoc][:to]
+      dst_suffix = if FORMAT_TO_EXT.keys.include? to then FORMAT_TO_EXT[to] else to end
       pandoc = Paru::Pandoc.new
       target[:pandoc].each do |option, value|
         pandoc.send option, value
       end
       dst = File.join dst_dir, "#{src_base}.#{dst_suffix}"
       pandoc.output dst
-      pandoc << File.read(src_file)
+      pandoc << File.read(src)
     end
 
   end
